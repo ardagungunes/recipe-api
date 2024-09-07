af4775f2c72007f41b7e2223f0e529035233e152
ardagungunes <ardababa.123@outlook.com>
2024-09-08 01:59:35 +0300

    Configured Docker compose to make it wait for database before service starts to run

---
+++ .history/app/core/management/commands/wait_for_db_20240907231033.py
@@ -0,0 +1,15 @@
+"""
+Django command to wait for the database to be available.
+"""
+import time
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.utils.db
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """Django command to wait for database."""
+    
+    def handle(self, *args, **options):
+        pass
